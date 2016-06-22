b6bbca640523c983120c7f5a31aa1ef40205d4f0
Bucquet Anthony <anthony@localhost.localdomain>
2016-06-22 23:42:04 +0200

    Added mini-script for class generation

---
+++ tools/newClass.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+# Usage : ../tools/newClass.sh <classname>
+
+if [ -z $1 ]; then
+	echo "Usage : ../tools/newClass.sh <classname>"
+else
+
+CLASSNAME=$1
+CCLASSNAME=$(echo $CLASSNAME | awk '{print toupper($0)}')
+HEADERNAME="$1.h"
+SOURCENAME="$1.cpp"
+
+
+# Generation header
+
+echo "#ifndef $CCLASSNAME"          >  $HEADERNAME
+echo "#define $CCLASSNAME"          >> $HEADERNAME
+echo ""                             >> $HEADERNAME
+echo "class $CLASSNAME {"           >> $HEADERNAME
+echo ""                             >> $HEADERNAME
+echo "};"                           >> $HEADERNAME
+echo ""                             >> $HEADERNAME
+echo "#endif // define $CCLASSNAME" >> $HEADERNAME
+
+# Generation source file
+
+echo "#include \"$HEADERNAME\""     >> $SOURCENAME
+echo ""                             >> $SOURCENAME
+
+fi
